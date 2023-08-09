5d16e7a795093dbd1e511d90e6ec4c8bf25ced16
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-08-09 19:35:48 +0530

    updated again

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoDB();
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://bhojanalay.netlify.app/")
+    res.setHeader("Access-Control-Allow-Origin", "https://bhojanalay.netlify.app")
     res.header(
         "Access-Control-Allow-Headers",
         "Origin , X-Requested-With, Content-Type, Accept"
