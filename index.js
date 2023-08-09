63a630d0799b4cd2afdaa8a9710e84a3b27bca57
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-08-09 19:31:57 +0530

    updated the access control

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoDB();
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://64d34617a4ea5b37f35733d1--bucolic-kulfi-4a9fe5.netlify.app")
+    res.setHeader("Access-Control-Allow-Origin", "https://bhojanalay.netlify.app/")
     res.header(
         "Access-Control-Allow-Headers",
         "Origin , X-Requested-With, Content-Type, Accept"
